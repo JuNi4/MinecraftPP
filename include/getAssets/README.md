331211d5a4b24266c11da24b9f47ad008b59900e
JuNi4 <justus@hrtm.name>
2023-02-12 17:27:41 +0100

    Documentation

---
+++ include/getAssets/README.md
@@ -0,0 +1,36 @@
+## Minecraft++ assets library
+By: @JuNi4
+
+A simple library for downloading assets from Mojang
+
+-- Requirements --
+  - libzip   (`sudo apt install libzip-dev` or https://libzip.org/download/)
+  - curl     (`sudo apt install libcurl-dev` or https://curl.se/download.html)
+  - nlohmann/json (https://github.com/nlohmann/json)
+  - elnormous/HTTPRequest (https://github.com/elnormous/HTTPRequest)
+
+-- Quick help --
+(Because there are only three simple functions)
+`getAssets( std::string version, std::string base_path = "" )`
+Downloads the assets, contained in the version jar file, from Mojang
+`version` Being the Minecraft version to get the assets from
+`base_path` Is the place, where the assets will be downloaded to.
+
+`getResources( std::string version, std::string base_path = "" )`
+Downloads the Resources, the Minecraft launcher downloads. This may take up to 10 minutes or more, depending on your connection speed.
+`version` Being the Minecraft version to get the assets from
+`base_path` Is the place, where the assets will be downloaded to.
+
+Quick Demo:
+```cpp
+#include <getAssets/getAssets.hpp>
+
+int main() {
+    // Download the assets contained in the version jar file
+    getAssets("1.19.3", "assets/");
+    // Get the resources, downloaded by the launcher. This may take up to 10 minutes or more
+    getResources("1.19.3", "assets/");
+
+    return 0;
+}
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,12 @@
 #include <nlohmann/json.hpp>
 #include <iostream>
 
-#include <options.hpp>
-#include <getAssets.hpp>
+#include <options/options.hpp>
+#include <getAssets/getAssets.hpp>
 
 using json = nlohmann::json;
 
 int main() {
-    // -- Demo Options file creator --
-    // delete the option files (only if they exist)
-    options::deleteOptionsFile();
-    options::deleteOptionsFile(options::defaultKeyBindsPath());
-
-    // Check if optionsfile exists
-    if ( ! std::filesystem::is_regular_file( options::defaultOptionsPath() )) {
-        // If the mc options file exists: import it
-        if (std::filesystem::is_regular_file(options::getMcPath()+"options.txt")) {
-            options::importConfig();
-        } else { // otherwise, create new files
-            // Crate Options file
-            options::createOptionsFile();
-            // Create key binds file
-            options::createOptionsFile(false, options::defaultKeyBindsPath(), options::defaultKeyBindStruct);
-        }
-    } else if ( ! std::filesystem::is_regular_file( options::defaultKeyBindsPath() ) ) {
-        // Create key binds file
-        options::createOptionsFile(false, options::defaultKeyBindsPath(), options::defaultKeyBindStruct);
-    }
-    // load the two files
-    json opt = options::getOptions();
-    json keys = options::getOptions(options::defaultKeyBindsPath());
-
-    // get the title of the window
-    std::cout << opt["title"] << std::endl;
-
-    // set the title directly
-    options::setOption("title", "Minecraft--");
-
-    // set the title inside of the json object
-    opt["title"] = "Minecraft**";
-
-    // save the opt json object
-    options::saveOptions(opt);
-
-    // -- End of Demo --
+    getAssets("1.19.3");
     return 0;
 }
