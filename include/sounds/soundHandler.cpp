88ec08c1dcca441a4c5a82d93276b81741f7ce4b
JuNi4 <81480623+JuNi4@users.noreply.github.com>
2023-02-17 10:12:25 +0000

    sound handler

--- include/sounds/soundHandler.cpp
+++ include/sounds/soundHandler.cpp
@@ -3,7 +3,7 @@
 
 soundHandler::soundHandler() {}
 
-void soundHandler::_player(int id, std::string soundId, float pitch, bool 3DAudio, sf::Vector3f pos(0.0f, 0.0f, 0.0f)) {
+void soundHandler::_player(int id, std::string soundId, float pitch, bool enable3D, sf::Vector3f pos) {
     // create a sound object
     sound sfx;
 
@@ -14,26 +14,35 @@ void soundHandler::_player(int id, std::string soundId, float pitch, bool 3DAudi
     sfx.setPitch(pitch);
 
     // set 3D audio
-    sfx.enable3DAudio(3DAudio);
+    sfx.enable3DAudio(enable3D);
 
     // play sound
     sdx.play();
 
     // keep sound alive
-    while (sfx.getStatus() = sf::SoundSource::Status::Playing) {
-        if (3DAudio) {
+    while (sfx.getStatus() = sf::SoundSource::Status::Playing && ! std::find( this->stop.begin, this->stop.end, id ) != this->stop.end ) {
+        if (enable3D) {
             sfx.pos = pos - this->_player;
         }
         float vol = this->_masterVolume/100 * this->_musicVolume/100;
         sfx.setVolume(vol);
     }
+
+    // remove id from stop
+    if (std::find( this->stop.begin, this->stop.end, id ) != this->stop.end ) {
+        std::remove( this->stop.begin, this->stop.end, std::find( this->stop.begin, this->stop.end, id ) );
+    }
+
+    // remove id from running
+    std::remove( this->running.begin, this->running.end, std::find( this->running.begin, this->running.end, id ) );
+
 }
 
-int soundHandler::call(std::string soundId, float pitch, bool 3DAudio, sf::Vector3f pos(0.f, 0.f, 0.f)) {
+int soundHandler::call(std::string soundId, float pitch, bool enable3D, sf::Vector3f pos) {
     // Increment the last id counter
     this->_lastId++;
     // run the sound asyncronisly
-    std::future<void> ft = std::async(this->_player, this->_lastId, soundId, pitch, 3DAudio, pos);
+    std::future<void> ft = std::async(this->_player, this->_lastId, soundId, pitch, enable3D, pos);
     // add id to running
     this->running.pushback(this->_lastId);
     // return ID
--- include/sounds/soundHandler.h
+++ include/sounds/soundHandler.h
@@ -1,31 +1,32 @@
 #include "sound.h"
+#include <SFML/Audio/Music.hpp>
 
 #ifndef _SOUND_HANDLER_H_
 #define _SOUND_HANDLER_H_
 
 class soundHandler {
     private:
-        sf::Vector3f _player(0.0f, 0.0f, 0.0f);
+        sf::Vector3f _player;
 
         int _masterVolume = 50;
         int _musicVolume = 100;
 
         int _lastId = -1;
 
+        int stop[256];
+        int running[256];
+
         /**
          * @brief The audio player function
          */
-        void _player(int id, std::string sound_id, float pitch, bool 3DAudio, sf::Vector3f pos(0.0f, 0.0f, 0.0f));
+        void _player(int id, std::string sound_id, float pitch, bool enable3D, sf::Vector3f pos);
     public:
-        char* stop[];
-        char* running[];
-
         /**
          * @brief Construct a new sound Handler
          */
         soundHandler();
 
-        int call(std::string soundId, float pitch, bool 3DAudio, sf::Vector3f pos(0.f, 0.f, 0.f));
+        int call(std::string soundId, float pitch, bool enable3D, sf::Vector3f pos = sf::Vector3f (0.f, 0.f, 0.f));
 }
 
 #endif
